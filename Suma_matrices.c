d204cfa8a31220513fd49a6d8add150f45f58719
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2023-11-28 15:20:30 -0500

    Ultimo commit: Se crean los comentarios del codigo

--- Suma_matrices.c
+++ Suma_matrices.c
@@ -1,28 +1,28 @@
-#include <stdio.h>  //Biblioteca principal//
-#include <stdlib.h> //Biblioteca para los numeros random//
-#include <time.h>   //Biblioteca para la generacion de numeros random//
+#include <stdio.h>  //Biblioteca principal
+#include <stdlib.h> //Biblioteca para los numeros random
+#include <time.h>   //Biblioteca para la generacion de numeros random
 
 int main()
-{ // Funcion principal//
+{ // Funcion principal
 
-    int a, b; // Definir como enteros las variables para las filas y columnas de la matriz//
+    int a, b; // Definir como enteros las variables para las filas y columnas de la matriz
     printf("Ingrese el valor de la fila de la matriz: ");
-    scanf("%d", &a); // Se pide y se escanea al usuario el numero de las filas//
+    scanf("%d", &a); // Se pide y se escanea al usuario el numero de las filas
     printf("Ingrese el valor de la columna de la matriz: ");
-    scanf("%d", &b);   // Se pide y se escanea al usuario el numero de las columnas//
-    int matrizA[a][b]; // Se define la matriz con la que vamos a trabajar //
+    scanf("%d", &b);   // Se pide y se escanea al usuario el numero de las columnas
+    int matrizA[a][b]; // Se define las matrices A, B y matriz resultado
     int matrizB[a][b];
     int matrizR[a][b];
-    srand(time(NULL)); // Se crea la funcion para crear numeros randoms//
+    srand(time(NULL)); // Se crea la funcion para crear numeros randoms
     printf("Matriz A");
-    for (int i = 0; i < a; i++) // Se crea el primer for para la creacion de la matriz, para el uso de filas//
+    for (int i = 0; i < a; i++)
     {
-        printf("\n");               // Se crea para separar a la siguiente linea las filas//
-        for (int j = 0; j < b; j++) // Se crea el segundo for para la creacion de la matriz, para el uso de las columnas//
+        printf("\n"); // Se crea para separar a la siguiente linea las filas
+        for (int j = 0; j < b; j++)
         {
-            matrizA[i][j] = rand() % 100 + 1; // Se define que en la matriz se agreguen numeros random en cada pocision de la matriz//
+            matrizA[i][j] = rand() % 100 + 1; // Se define que en la matriz A se agreguen numeros random en cada pocision de la matriz
 
-            printf("%d \t", matrizA[i][j]); // Se imprime la matriz//
+            printf("%d \t", matrizA[i][j]); // Se imprime la matriz A
         }
     }
     printf("\n");
@@ -32,8 +32,8 @@ int main()
         printf("\n");
         for (int j = 0; j < b; j++)
         {
-            matrizB[i][j] = rand() % 100 + 1;
-            printf("%d \t", matrizB[i][j]);
+            matrizB[i][j] = rand() % 100 + 1; // De igual forma que en la matriz A, se crea la matriz B con numeros aleatorios dentro de la matriz
+            printf("%d \t", matrizB[i][j]);   // Se imprime la matriz B
         }
     }
 
@@ -42,10 +42,10 @@ int main()
     for (int i = 0; i < a; i++)
     {
         printf("\n");
-        for (int j = 0; j < b; j++)
+        for (int j = 0; j < b; j++) // Se crea la matriz resultante
         {
-            matrizR[i][j] = matrizA[i][j] + matrizB[i][j];
-            printf("%d \t", matrizR[i][j]);
+            matrizR[i][j] = matrizA[i][j] + matrizB[i][j]; // Se suma la matriz A y matriz B en la matriz resultante
+            printf("%d \t", matrizR[i][j]);                // Se imprime la matriz resultante
         }
     }
 
